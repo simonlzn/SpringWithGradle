f1352b69b31f94b4c323d33da2423510f7495121
Zinan Lin <simonlininter@yahoo.com>
2016-01-18 16:10:36 +0800

    init

---
+++ src/main/java/WebConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="controller")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+
+        resolver.setPrefix("/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
